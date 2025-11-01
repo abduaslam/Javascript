af500c387bba83cede55f7724463d516d33f45ed
abduaslam <abduaslam098@gmail.com>
2025-11-01 12:24:12 +0200

    my dom practice

---
+++ Dom/EventLitsners.js
@@ -0,0 +1,8 @@
+//excute event listener using function 
+let btn2=document.querySelector(".box2");
+
+ function greeting(){
+alert('Hello my friend this is my first event listener');
+
+};
+btn2.addEventListener(onclick,greeting)
--- Dom/creatingElement.js
+++ Dom/creatingElement.js
@@ -18,4 +18,26 @@ newElement3.textContent='my third list element '
 
 // making some style
 newElement.style.backgroundColor='#f2d'
+newElement1.style.fontSize='20px'
+let items=document.querySelectorAll('.element')
+console.log(items);
+items.forEach(x=>x.style.backgroundColor='#de4')
+
+// modfiying attributes (add and remove class or name)
+newElement.classList.add('myOrderList')
+// add class name for each li element
+const liItems=document.querySelectorAll('li');
+liItems.forEach(x=>x.classList.add('Order-element'))
+
+
+// remove attribute
+newElement1.removeAttribute("class")
+
+
+
+//remove element
+newElement2.remove()
+
+
+
 
