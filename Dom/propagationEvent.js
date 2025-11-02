da9b55a3c52c9274d9caca5df615b898a024702b
abduaslam <abduaslam098@gmail.com>
2025-11-02 12:30:36 +0200

    dom js practices

--- Dom/propagationEvent.js
+++ Dom/propagationEvent.js
@@ -1,16 +1,23 @@
 let container=document.querySelector(".container");
-let ul=document.querySelector(".parent-list");
-let li=document.querySelector(".ul-child");
+// let ul=document.querySelector(".parent-list");
+// let li=document.querySelector(".ul-child");
 
 
-li.addEventListener('click',()=>{
-    console.log('liClicked')
+// li.addEventListener('click',()=>{
+//     console.log('liClicked')
+// })
+// ul.addEventListener('click',()=>{
+//     console.log('ulClicked')
+// },true)
+
+
+// container.addEventListener('click',(event)=>{
+//     event.stopPropagation() // stop ropagation
+//     console.log('conainerClicked')
+// },true)
+
+
+// anthor example
+container.addEventListener('click',(event)=>{
+console.log(event.target.style.color='red')
 })
-ul.addEventListener('click',()=>{
-    console.log('ulClicked')
-},true)
-
-
-container.addEventListener('click',()=>{
-    console.log('conainerClicked')
-},true)
