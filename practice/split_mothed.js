e4d4cf9932089ac7bda47b354f99252a3b6a2509
abduaslam <abduaslam098@gmail.com>
2025-11-11 08:45:16 +0200

    remove file

---
+++ practice/split_mothed.js
@@ -0,0 +1,7 @@
+let str="hello"
+console.log(str.split(''))
+let word ="mango, orange";
+let result=word.split(',')
+console.log(result)
+console.log(result.reverse())
+console.log(result.reverse().join(' '))
