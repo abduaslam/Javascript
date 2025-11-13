740c2fec700a4b4edf124d6eb89e8a9f666e30b5
abduaslam <abduaslam098@gmail.com>
2025-11-13 15:39:53 +0200

    fix Js files

---
+++ Dom/creatingElement 2.js
@@ -0,0 +1,8 @@
+let contain=document.getElementById("contain");
+
+let div=document.createElement("div");
+let text= div.innerHTML='thie is added '
+
+contain.append(div);
+console.log(contain);
+console.log(div)
